a09f25b10bbd9ba031fb16030f571faf87dfcf05
Shalu Jose <58412871+ShaluJose18@users.noreply.github.com>
2019-12-04 12:27:04 +0530

    Add files via upload
    Money trasfer failure status updated

---
+++ SQL_failureStatus.sql
@@ -0,0 +1,52 @@
+drop procedure st_transaction_rollback;
+
+/* Procedure */
+ Delimiter //
+ Create Procedure st_transaction_rollback(
+ in _with_amount double,
+ in _sender_accno varchar(30),
+ in _receiver_accno varchar(30)
+ )
+     begin
+     declare _samount double;
+     declare _ramount double;
+
+     declare exit handler for sqlexception
+     begin
+     select 'Transaction Failed' as message;
+     rollback;
+     end;
+     declare exit handler for sqlwarning
+     begin
+     rollback;
+     end;
+     
+     
+    start transaction;
+     
+     insert into transactions(sender_accno,receiver_accno,amount) values((select user_id from user_details where account_no =  _sender_accno),(select user_id from user_details where account_no =_receiver_accno),_with_amount);
+
+     select b.amount into _samount from user_details as u inner join balance_info as b on u.user_id = b.userdetails_id where u.account_no=_sender_accno;
+     select _samount;
+     if(_samount is not null) then
+	 update balance_info set amount=amount - _with_amount where userdetails_id=(select user_id from user_details where account_no =  _sender_accno) and amount >= _with_amount;
+     else
+     select ('No such customer'); 
+     rollback;
+     end if;
+     select 'reciever';
+     select b.amount into _ramount from user_details as u inner join balance_info as b on u.user_id = b.userdetails_id where u.account_no=_receiver_accno;
+     select _ramount;
+     if(_ramount is not null) then
+	 update balance_info set amount=amount + _with_amount where userdetails_id=(select user_id from user_details where account_no =_receiver_accno); 
+     update transactions set status='Success' where sender_accno=(select user_id from user_details where account_no =  _sender_accno);
+     select 'Your transaction successfully completed' as message;
+     else
+		 rollback;
+     end if;
+		commit;
+	
+     end //
+     
+DELIMITER ;
+call st_transaction_rollback (100,'SBI1200456','SBI1200789')
